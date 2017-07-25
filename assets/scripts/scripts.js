06bc772964c9a7864a75a78b4f649934594aa64d
Sergey Glazov <yaglazov@gmail.com>
2017-07-25 16:14:52 +0300

    Update 2017-07-25T13:14:11.570Z

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -121,6 +121,32 @@ if ($) $(function () {
   });
 });
 // /подсвечиваем ссылки с одинаковым адресом
+'use strict';
+
+$(document).ready(function () {
+
+  // спойлер-блок на странице
+  $('._foldable__content').hide();
+  $('._foldable__control').click(function () {
+    $(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
+    var close = $('._foldable__link').attr("data-text-close");
+    var open = $('._foldable__link').attr("data-text-open");
+    if ($(this).hasClass('folded')) {
+      if (close) {
+        $('._foldable__link').html(close);
+      } else {
+        $('._foldable__link').html('Свернуть раскрывающийся контент');
+      }
+    } else {
+      if (open) {
+        $('._foldable__link').html(open);
+      } else {
+        $('._foldable__link').html('Показать раскрывающийся контент');
+      }
+    }
+  });
+  // /спойлер-блок на странице
+});
 
 /*
  By Osvaldas Valutis, www.osvaldas.info
@@ -156,142 +182,3 @@ if ($) $(function () {
 $('#commentnew').keyup(function () {
   if ($(this).val().length != 0) $('#submit').attr('disabled', false);else $('#submit').attr('disabled', true);
 });
-'use strict';
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var ProgressCircle = function () {
-  function ProgressCircle(percent, radius, elementClass) {
-    _classCallCheck(this, ProgressCircle);
-
-    this._percent = percent;
-    this._radius = radius;
-    this._elementClass = elementClass;
-  }
-
-  _createClass(ProgressCircle, [{
-    key: 'calcDashOffset',
-    value: function calcDashOffset() {
-      var circumference = Math.PI * (2 * this.radius);
-      return Math.floor(circumference - this.percent / 100 * circumference);
-    }
-  }, {
-    key: 'createCSS',
-    value: function createCSS() {
-      document.querySelectorAll('.' + this._elementClass + ' .donut__svg .donut__svg__circle--one')[0].style.strokeDashoffset = this.calcDashOffset();
-    }
-
-    // updateText() {
-    //   document.querySelectorAll(`.${this.elementClass} .js-donut-figure`)[0].innerText = this.percent;
-    // }
-
-  }, {
-    key: 'updateFigure',
-    value: function updateFigure(newStat) {
-      this._percent = newStat;
-      this.updateText();
-      this.createCSS();
-    }
-  }, {
-    key: 'init',
-    value: function init() {
-      var _this = this;
-
-      // this.updateText();
-
-      setTimeout(function () {
-        _this.createCSS();
-      }, 1000);
-    }
-  }, {
-    key: 'percent',
-    get: function get() {
-      return this._percent;
-    }
-  }, {
-    key: 'radius',
-    get: function get() {
-      return this._radius;
-    }
-  }, {
-    key: 'elementClass',
-    get: function get() {
-      return this._elementClass;
-      return document.getElementsByClassName(this._elementClass)[0];
-    }
-  }]);
-
-  return ProgressCircle;
-}();
-
-var progress = new ProgressCircle(47, 90, 'donut');
-progress.init();
-'use strict';
-
-// Сворачиваем и разворачиваем список уроков
-
-(function () {
-
-  $('._more-link').click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass("more-link--expand");
-    $(this).parents('.course-module').find('._course-module__items').toggleClass("folded").toggleClass("unfolded");
-
-    var close = $(this).attr("data-text-close");
-    var open = $(this).attr("data-text-open");
-
-    if ($(this).parents('.course-module').find('._course-module__items').hasClass('folded')) {
-      if (close) {
-        $(this).html(close);
-      } else {
-        $(this).html('Свернуть уроки');
-      }
-    } else {
-      if (open) {
-        $(this).html(open);
-      } else {
-        $(this).html('Показать уроки');
-      }
-    }
-  });
-
-  $('.course-module').each(function () {
-    var $e = $(this);
-    if ($e.find('._course-module__items').hasClass('folded')) {
-      $e.find('._more-link').toggleClass("more-link--expand").html('Свернуть уроки');
-    }
-  });
-})();
-
-(function () {
-
-  $('.comment').each(function () {
-    var $e = $(this);
-    $e.find('._comment__answer').hide();
-  });
-
-  $('._more-comment').click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass("more-link--expand");
-    $(this).parents('.comment').find('._comment__answer').toggleClass("folded").toggleClass("unfolded").slideToggle();
-
-    var close = $(this).attr("data-text-close");
-    var open = $(this).attr("data-text-open");
-
-    if ($(this).parents('.comment').find('._comment__answer').hasClass('folded')) {
-      if (close) {
-        $(this).html(close);
-      } else {
-        $(this).html('Скрыть ответ');
-      }
-    } else {
-      if (open) {
-        $(this).html(open);
-      } else {
-        $(this).html('Показать ответ');
-      }
-    }
-  });
-})();
