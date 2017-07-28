020f596e04657b0616bb7143fa2f88ed906a872f
Sergey Glazov <yaglazov@gmail.com>
2017-07-28 14:39:19 +0300

    Update 2017-07-28T11:39:08.128Z

--- assets/desktop/scripts/scripts.js
+++ assets/desktop/scripts/scripts.js
@@ -295,3 +295,9 @@ progress.init();
     }
   });
 })();
+'use strict';
+
+var pass = $(".psdfdsf");
+$('.show-password').click(function () {
+  pass.attr('type', pass.attr('type') === 'password' ? 'text' : 'password');
+});
