df9174882432557dbc0ac0eaa70476e3cd093854
Sergey Glazov <yaglazov@gmail.com>
2017-08-15 15:43:42 +0300

    объеденил результаты в один блок, буде играть с ним

--- desktop/templates/blocks/header/search.js
+++ desktop/templates/blocks/header/search.js
@@ -6,16 +6,16 @@
       $('.search-bubble').removeClass('show');
     } else {
       $('.search-bubble').addClass('show');
-      $('.search-bubble__row').hide();
+      $('.search-bubble__results').hide();
       $('.search-bubble__text').addClass('show');
     }
 
     if($(this).val().length > 1 ) {
       if ($(this).val() === 'кон') {
-        $('.search-bubble__row').show();
+        $('.search-bubble__results').show();
         $('.search-bubble__text').removeClass('show');
       } else {
-        $('.search-bubble__row').hide();
+        $('.search-bubble__results').hide();
         $('.search-bubble__text').addClass('show');
         $('.search-bubble__text h3').text('Ничего не найдено');
         $('.search-bubble__text p').text('Проверте написание или попробуйте другой запрос ');
