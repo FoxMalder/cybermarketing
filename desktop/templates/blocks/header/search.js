68019da84e7c75c8fa9ed6328253f2e82741aade
Sergey Glazov <yaglazov@gmail.com>
2017-08-15 15:36:16 +0300

    ещё порядок в js и стили пузыря поиска

--- desktop/templates/blocks/header/search.js
+++ desktop/templates/blocks/header/search.js
@@ -2,23 +2,23 @@
 
   $('#search').keyup(function() {
 
-
-
-    if($(this).val().length === 1 ) {
+    if($(this).val().length < 1 ) {
+      $('.search-bubble').removeClass('show');
+    } else {
       $('.search-bubble').addClass('show');
       $('.search-bubble__row').hide();
       $('.search-bubble__text').addClass('show');
-    } else {
-      if($(this).val().length > 1 ) {
-        if ($(this).val() === 'кон') {
-          $('.search-bubble__row').show();
-          $('.search-bubble__text').removeClass('show');
-        } else {
-          $('.search-bubble__row').hide();
-          $('.search-bubble__text').addClass('show');
-          $('.search-bubble__text h3').text('Ничего не найдено');
-          $('.search-bubble__text p').text('Проверте написание или попробуйте другой запрос ');
-        }
+    }
+
+    if($(this).val().length > 1 ) {
+      if ($(this).val() === 'кон') {
+        $('.search-bubble__row').show();
+        $('.search-bubble__text').removeClass('show');
+      } else {
+        $('.search-bubble__row').hide();
+        $('.search-bubble__text').addClass('show');
+        $('.search-bubble__text h3').text('Ничего не найдено');
+        $('.search-bubble__text p').text('Проверте написание или попробуйте другой запрос ');
       }
     }
 
