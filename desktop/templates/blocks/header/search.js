b5ac9e7e7f918c0ca26cba5467146a201ab4a5e7
Sergey Glazov <yaglazov@gmail.com>
2017-08-15 15:30:22 +0300

    добавил демо-фотки в поиск, почти победил js-штуку для поиска

---
+++ desktop/templates/blocks/header/search.js
@@ -0,0 +1,27 @@
+(function(){
+
+  $('#search').keyup(function() {
+
+
+
+    if($(this).val().length === 1 ) {
+      $('.search-bubble').addClass('show');
+      $('.search-bubble__row').hide();
+      $('.search-bubble__text').addClass('show');
+    } else {
+      if($(this).val().length > 1 ) {
+        if ($(this).val() === 'кон') {
+          $('.search-bubble__row').show();
+          $('.search-bubble__text').removeClass('show');
+        } else {
+          $('.search-bubble__row').hide();
+          $('.search-bubble__text').addClass('show');
+          $('.search-bubble__text h3').text('Ничего не найдено');
+          $('.search-bubble__text p').text('Проверте написание или попробуйте другой запрос ');
+        }
+      }
+    }
+
+  });
+
+})();
