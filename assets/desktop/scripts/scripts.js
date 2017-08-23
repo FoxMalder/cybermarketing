70f67cab5edd2619cf6d10cf804bcb5223bba4b9
Sergey Glazov <yaglazov@gmail.com>
2017-08-23 19:16:27 +0300

    Update 2017-08-23T16:15:53.366Z

--- assets/desktop/scripts/scripts.js
+++ assets/desktop/scripts/scripts.js
@@ -121,48 +121,6 @@ if ($) $(function () {
   });
 });
 // /подсвечиваем ссылки с одинаковым адресом
-'use strict';
-
-(function () {
-
-  $('#search').keyup(function () {
-
-    if ($(this).val().length < 1) {
-      $('._search-bubble').removeClass('show');
-      $(this).removeClass('focus');
-      $('.search__box .loading').hide();
-      $('.search__box span .ico-cross').hide();
-    } else {
-      $(this).addClass('focus');
-      $('.search__box .loading').show();
-      $('._search-bubble').addClass('show');
-      $('._search-results').removeClass('show');
-      $('._search-bubble__text').addClass('show');
-      $('._search-bubble__text h3').text('Одной буквы недостаточно для поиска');
-      $('._search-bubble__text p').text('Попробуйте ввести слово целиком?');
-    }
-
-    if ($(this).val().length > 1) {
-      if ($(this).val() === 'кон') {
-        $('._search-results').addClass('show');
-        $('._search-bubble__text').removeClass('show');
-        $('.search__box .loading').hide();
-        $('.search__box span .ico-cross').show();
-      } else {
-        $('._search-results').removeClass('show');
-        $('._search-bubble__text').addClass('show');
-        $('._search-bubble__text h3').text('Ничего не найдено');
-        $('._search-bubble__text p').text('Проверте написание или попробуйте другой запрос');
-      }
-    }
-  });
-})();
-'use strict';
-
-var pass = $(".psdfdsf");
-$('.show-password').click(function () {
-  pass.attr('type', pass.attr('type') === 'password' ? 'text' : 'password');
-});
 
 /*
  By Osvaldas Valutis, www.osvaldas.info
@@ -834,3 +792,45 @@ $('.info-mini .circle').circleProgress({
     }
   });
 })();
+'use strict';
+
+(function () {
+
+  $('#search').keyup(function () {
+
+    if ($(this).val().length < 1) {
+      $('._search-bubble').removeClass('show');
+      $(this).removeClass('focus');
+      $('.search__box .loading').hide();
+      $('.search__box span .ico-cross').hide();
+    } else {
+      $(this).addClass('focus');
+      $('.search__box .loading').show();
+      $('._search-bubble').addClass('show');
+      $('._search-results').removeClass('show');
+      $('._search-bubble__text').addClass('show');
+      $('._search-bubble__text h3').text('Одной буквы недостаточно для поиска');
+      $('._search-bubble__text p').text('Попробуйте ввести слово целиком?');
+    }
+
+    if ($(this).val().length > 1) {
+      if ($(this).val() === 'кон') {
+        $('._search-results').addClass('show');
+        $('._search-bubble__text').removeClass('show');
+        $('.search__box .loading').hide();
+        $('.search__box span .ico-cross').show();
+      } else {
+        $('._search-results').removeClass('show');
+        $('._search-bubble__text').addClass('show');
+        $('._search-bubble__text h3').text('Ничего не найдено');
+        $('._search-bubble__text p').text('Проверте написание или попробуйте другой запрос');
+      }
+    }
+  });
+})();
+'use strict';
+
+var pass = $(".psdfdsf");
+$('.show-password').click(function () {
+  pass.attr('type', pass.attr('type') === 'password' ? 'text' : 'password');
+});
