f579bd3be3899fea98bdab7311de49ff8a2914eb
Sergey Glazov <yaglazov@gmail.com>
2017-09-23 21:23:43 +0300

    Update 2017-09-23T18:23:40.310Z

--- assets/desktop/scripts/scripts.js
+++ assets/desktop/scripts/scripts.js
@@ -273,90 +273,16 @@ function px2px() {
   'use strict';
 
   var doc = document;
-  var panelClass = 'controls-panel';
-  var prefix = 'pg';
+  var panelClass = 'px2px-controls';
+  var panelClassInner = 'px2px-controls_inner';
 
   var controlsPanel = void 0;
+  var controlsPanelInner = void 0;
+
+  var marginLeftInput = void 0;
+  var onOff = void 0;
 
   var px2pxBlock = doc.getElementsByClassName('px2px')[0];
-
-  function setClasses(elem, classes) {
-    if (!elem) {
-      return;
-    }
-
-    if (classes.length > 0) {
-      classes.forEach(function (className) {
-        elem.classList.add(className);
-      });
-    }
-  }
-
-  function saveLocalStorage(name, value) {
-    var itemName = [prefix, name].join('-');
-    localStorage[itemName] = value;
-  }
-
-  function createDragButton() {
-    var input = doc.createElement('button');
-    setClasses(input, [panelClass + '__control', panelClass + '__control--drag-n-drop']);
-    input.setAttribute('type', 'button');
-    input.innerHTML = ' ';
-
-    controlsPanel.appendChild(input);
-
-    input.onmousedown = function () {
-      //Place it here to get real sizes after
-      // external styles has been loaded
-      var offsetTop = this.offsetTop;
-      var offsetLeft = controlsPanel.clientWidth - this.clientWidth;
-      var styles = getComputedStyle(controlsPanel);
-
-      controlsPanel.style.top = styles.top;
-      controlsPanel.style.left = styles.left;
-      controlsPanel.style.right = 'auto';
-      controlsPanel.style.bottom = 'auto';
-
-      doc.onmousemove = function (ev) {
-        var x = ev.clientX - offsetLeft + 'px';
-        var y = ev.clientY + 'px';
-
-        controlsPanel.style.left = x;
-        controlsPanel.style.top = y;
-      };
-    };
-
-    input.onmouseup = function () {
-      var styles = getComputedStyle(controlsPanel);
-      var left = +styles.left.replace(/px/, '');
-      var right = +styles.right.replace(/px/, '');
-      var top = +styles.top.replace(/px/, '');
-      var bottom = +styles.bottom.replace(/px/, '');
-
-      if (left > right) {
-        saveLocalStorage('left', 'auto');
-        saveLocalStorage('right', styles.right);
-
-        controlsPanel.style.right = styles.right;
-        controlsPanel.style.left = 'auto';
-      } else {
-        saveLocalStorage('left', styles.left);
-        saveLocalStorage('right', 'auto'); //'auto' needs to override default position;
-      }
-      if (top > bottom) {
-        saveLocalStorage('top', 'auto');
-        saveLocalStorage('bottom', styles.bottom);
-
-        controlsPanel.style.bottom = styles.bottom;
-        controlsPanel.style.top = 'auto';
-      } else {
-        saveLocalStorage('top', styles.top);
-        saveLocalStorage('bottom', 'auto');
-      }
-
-      doc.onmousemove = null;
-    };
-  }
 
   function createContolsPanel() {
     var targetElem = doc.documentElement;
@@ -364,9 +290,26 @@ function px2px() {
     controlsPanel = doc.createElement('div');
     controlsPanel.classList.add(panelClass);
     targetElem.appendChild(controlsPanel);
-    var sides = ['top', 'right', 'bottom', 'left'];
 
-    createDragButton();
+    controlsPanelInner = doc.createElement('div');
+    controlsPanelInner.classList.add(panelClassInner);
+    controlsPanel.appendChild(controlsPanelInner);
+
+    marginLeftInput = doc.createElement('input');
+    marginLeftInput.type = "text";
+    marginLeftInput.name = "marginleft";
+    marginLeftInput.value = "";
+    marginLeftInput.id = "marginleft";
+    marginLeftInput.placeholder = "marginLeftControl";
+
+    controlsPanelInner.appendChild(marginLeftInput);
+
+    onOff = doc.createElement('input');
+    onOff.type = "checkbox";
+    onOff.id = "onOffControl";
+    onOff.checked = true;
+
+    controlsPanelInner.appendChild(onOff);
   }
 
   if (px2pxBlock) {
@@ -379,6 +322,10 @@ function px2px() {
 
 window.onload = function () {
   px2px();
+
+  document.getElementById('onOffControl').onchange = function () {
+    document.getElementsByClassName('px2px')[0].style.display = this.checked ? 'block' : 'none';
+  };
 };
 
 /*
